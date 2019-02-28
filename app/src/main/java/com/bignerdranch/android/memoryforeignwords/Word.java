583992912ca79a5ac7fd7a6467ff791e3e09b6a8
tompm1 <tomasz.for@gmail.com>
2019-02-28 21:37:53 +0100

    test1

--- app/src/main/java/com/bignerdranch/android/memoryforeignwords/Word.java
+++ app/src/main/java/com/bignerdranch/android/memoryforeignwords/Word.java
@@ -1,4 +1,47 @@
 package com.bignerdranch.android.memoryforeignwords;
 
+import java.util.UUID;
+
 public class Word {
+
+    private UUID mId;
+    private String mOriginalWord;
+    private String mTranslatedWord;
+    private int mScore;
+
+    public Word() {
+        this.mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public void setmId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getmOriginalWord() {
+        return mOriginalWord;
+    }
+
+    public void setmOriginalWord(String mOriginalWord) {
+        this.mOriginalWord = mOriginalWord;
+    }
+
+    public String getmTranslatedWord() {
+        return mTranslatedWord;
+    }
+
+    public void setmTranslatedWord(String mTranslatedWord) {
+        this.mTranslatedWord = mTranslatedWord;
+    }
+
+    public int getmScore() {
+        return mScore;
+    }
+
+    public void setmScore(int mScore) {
+        this.mScore = mScore;
+    }
 }
---
+++ app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordFragment.java
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.memoryforeignwords;
+
+import android.support.v4.app.Fragment;
+
+public class WordFragment extends Fragment {
+}
