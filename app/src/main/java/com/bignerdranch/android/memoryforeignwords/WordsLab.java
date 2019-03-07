9433945e00dd3e54c35175f94ed568dd308c673b
tompm1 <tomasz.for@gmail.com>
2019-03-07 21:39:57 +0100

    test1

--- app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordsLab.java
+++ app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordsLab.java
@@ -1,4 +1,20 @@
 package com.bignerdranch.android.memoryforeignwords;
 
+import android.content.Context;
+
 public class WordsLab {
+
+    private static WordsLab sWordsLab;
+
+    private WordsLab (Context context){
+        
+    }
+
+    public static WordsLab get (Context context){
+        if(sWordsLab == null){
+            sWordsLab = new WordsLab(context);
+        }
+        return sWordsLab;
+    }
+
 }
