bbba257939a5b1ac194d88d25ca91904bcd4af7c
tompm1 <tomasz.for@gmail.com>
2019-03-08 20:39:08 +0100

    test1

--- app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordActivity.java
+++ app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordActivity.java
@@ -1,25 +1,12 @@
 package com.bignerdranch.android.memoryforeignwords;
 
+
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class WordActivity extends AppCompatActivity {
+public class WordActivity extends SingleFragmentActivity{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_word);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if (fragment == null){
-            fragment = new WordFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-
-        }
+    protected Fragment createFragment(){
+        return new WordFragment();
     }
+
 }
--- app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordsLab.java
+++ app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordsLab.java
@@ -2,11 +2,23 @@ package com.bignerdranch.android.memoryforeignwords;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class WordsLab {
 
     private static WordsLab sWordsLab;
+    private List<Word> sWords;
 
     private WordsLab (Context context){
+        sWords = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Word word = new Word();
+            word.setmOriginalWord("OriginalWord #" +1);
+            word.setmTranslatedWord("TranslatedWord #" +1);
+            sWords.add(word);
+        }
 
     }
 
@@ -17,4 +29,12 @@ public class WordsLab {
         return sWordsLab;
     }
 
+    public Word getWord (UUID id){
+        for (Word word : sWords){
+            if (word.getmId().equals(id)){
+                return word;
+            }
+        }
+        return null;
+    }
 }
