f941df17e83d9445c9dc673847a02c95f6d26664
tompm1 <tomasz.for@gmail.com>
2019-03-17 17:57:20 +0100

    test1

--- app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordListActivity.java
+++ app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordListActivity.java
@@ -1,13 +1,17 @@
 package com.bignerdranch.android.memoryforeignwords;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class WordListActivity extends SingleFragmentActivity {
+public class WordListActivity extends SingleFragmentActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -19,10 +23,39 @@ public class WordListActivity extends SingleFragmentActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new WordFragment()).commit();
+                break;
+
+            case R.id.nav_search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SearchFragment()).commit();
+                break;
+
+            case R.id.nav_share:
+                Toast.makeText(this, "share", Toast.LENGTH_SHORT).show();
+
+            case R.id.nav_send:
+                Toast.makeText(this, "send", Toast.LENGTH_SHORT).show();
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     public void onBackPressed() {
@@ -32,7 +65,6 @@ public class WordListActivity extends SingleFragmentActivity {
             super.onBackPressed();
         }
     }
-
 
     protected Fragment createFragment() {
         return new WordListFragment();
--- app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordListFragment.java
+++ app/src/main/java/com/bignerdranch/android/memoryforeignwords/WordListFragment.java
@@ -4,22 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+
 
 public class WordListFragment extends Fragment {
+
+    private RecyclerView mWordsRecyclerView;
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-/*    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_word, container, false);
+        View view = inflater.inflate(R.layout.fragment_word_list, container, false);
         return view;
-    }*/
+    }
 
 }
